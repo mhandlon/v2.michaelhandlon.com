cf070060655a45714d7e9106f0085009ccdf4c09
Michael Handlon <michael.handlon@gmail.com>
2018-09-06 16:26:45 -0400

    save point

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/fontawesome-all.css';
 
 class App extends Component {
   render() {
