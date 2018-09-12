0b9765527e16f3dc3bb85b87fddbd0dadf53bb33
Michael Handlon <michael.handlon@gmail.com>
2018-09-12 00:01:00 -0400

    kinda drunk but need to backup

--- src/components/story.js
+++ src/components/story.js
@@ -3,13 +3,16 @@ import { Container, Row, Col } from 'reactstrap';
 
 import Storyhead from './storyhead';
 import StoryContent from './storycontent'
+import Contact from "./contact";
 
-//import '../css/storyhead.css'
-
-export default class Storyhead extends React.Component {
+export default class Story extends React.Component {
     render() {
         return (
+            <div>
             <Storyhead/>
+            <StoryContent/>
+                <Contact/>
+            </div>
         );
     }
 }
