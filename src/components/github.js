cf3d6b48e38ab8d6386dddb9f60b6bf4f88f3603
Michael Handlon <michael.handlon@gmail.com>
2019-03-24 22:48:29 -0400

    css fixes

--- src/components/github.js
+++ src/components/github.js
@@ -45,12 +45,12 @@ export default class Github extends React.Component {
         //console.log(user);
         const repos = this.state.repos;
         console.log("repos");
-        //console.log(repos);
+        console.log(repos);
 
         var myrepos = repos.map(repos =>
                 <Container className="myrepo-cont">
                     <Row className="myrepo">
-                        <a href="{repos.html_url}">
+                        <a className="repolink" href={repos.html_url}>
                             <i class="repoicon fab fa-github fa-3x float-left"></i>
                             <h1 class="repo-name float-left">{repos.name}</h1>
                         </a>
