c9aec92370f2aec042042b2c62f45c9c4c0aa94f
mhandlon <michael.handlon@gmail.com>
2025-11-16 10:22:17 -0500

    Clean up `App.js` by removing unused imports and commented-out code.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-//import ReactDOM from 'react-dom';
 
 import {
     BrowserRouter as Router,
-    //Link,
     Route,
 } from 'react-router-dom'
 
@@ -11,11 +9,10 @@ import SideBar from "./sidebar"
 import Home from "./home"
 import Story from "./story"
 import Github from "./github"
+// import Help from "./help"
 
 import '../css/global.css';
 import '../css/App.css';
-//import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-//import {slide as Menu} from "react-burger-menu";
 
 class App extends Component {
     constructor(){
@@ -23,7 +20,6 @@ class App extends Component {
           this.state = {
             results: {}
         };
-        //console.log(this.state.results);
     }
   render() {
     return (
@@ -33,6 +29,7 @@ class App extends Component {
             <Route exact path="/" component={Home}/>
             <Route exact path="/about" component={Story}/>
             <Route exact path="/open-source" component={Github}/>
+            {/*<Route exact path="/volunteer" component={Help}/>*/}
         </div>
         </Router>
     );
